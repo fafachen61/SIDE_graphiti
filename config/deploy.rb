b0cd40effa9df72f81de222e7a7cd68162e3c5e9
fafachen61 <fafachen.61@gmail.com>
2021-08-07 23:47:04 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,6 @@ lock "~> 3.16.0"
 
 set :application, "graffiti"
 set :repo_url, "git@github.com:fafachen61/SIDE_graphiti.git"
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -22,7 +21,7 @@ set :deploy_to, "/home/deploy/mygraffiti"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
